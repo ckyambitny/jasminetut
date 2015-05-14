e570a7a005c564fde6bb4c3215ae45eec5432079
Piotr Kowalski <piecioshka@gmail.com>
2015-05-14 20:50:59 +0200

    More improvements.

--- test/spec/test.numbers.js
+++ test/spec/test.numbers.js
@@ -1,25 +1,38 @@
-define(['numbers'], function (numbers) {
+define([
+    'numbers'
+], function (numbers) {
     'use strict';
 
     describe('Numbers module', function () {
         describe('The add method', function () {
-            // xit -same as pending(); , xdescribe - 0 specs
             it('accepts one or more args and returns sum of them', function () {
-                /*pending(); */ //marks spec as pending!
-
                 // arrange
-                var output,
-                    input1 = 1,
-                    input2 = 2;
+                var output;
+                var input1 = 1;
+                var input2 = 2;
+
                 // act
                 output = numbers.add(input1, input2);
+
                 // assert using matchers
                 expect(output).toEqual(3);
                 expect(output).not.toEqual(4);
-
             });
 
-            it()
+            it('should add two values', function () {
+                expect(numbers.add(1, 2)).toEqual(3);
+                expect(numbers.add(1, 0)).toEqual(1);
+                expect(numbers.add(0, 0)).toEqual(0);
+                expect(numbers.add(-10, 0)).toEqual(-10);
+                expect(numbers.add(-10, 20)).toEqual(10);
+            });
+
+            it('should add many values', function () {
+                expect(numbers.add(1)).toEqual(1);
+                expect(numbers.add(0)).toEqual(0);
+                expect(numbers.add(0, 0, 0, 0, 0, 0, 0, 0, 0)).toEqual(0);
+                expect(numbers.add(0, 0, 0, 0, 0, 10, 0, 0, 0)).toEqual(10);
+            });
         });
     });
 });

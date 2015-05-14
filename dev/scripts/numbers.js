80adc4d7c8223d003055fe63539239ec9a9c94f1
Bartlomiej Sobczuk <bartlomiej.sobczuk@gmail.com>
2015-05-14 18:24:20 +0200

    jasmine tutorial

---
+++ dev/scripts/numbers.js
@@ -0,0 +1,18 @@
+define( function () {
+	'use strict';
+	var self = {};
+
+
+	self.add = function add () {
+		var inputs = Array.prototype.slice.call(arguments),
+			total = 0;
+
+		inputs.forEach(function (value) {
+			total += value;
+		});
+		
+		return total;
+	}
+
+	return self;
+});
---
+++ test/spec/numbers.js
@@ -0,0 +1,30 @@
+define(['numbers'], function (numbers) {
+	'use strict';
+	describe('Numbers module', function () {
+		describe('The add method', function () {
+			// xit -same as pending(); , xdescribe - 0 specs
+			it('accepts one or more args and returns sum of them', function () {
+				/*pending(); */ //marks spec as pending!
+				
+				// arrange
+				var output,
+					input1 = 1,
+					input2 = 2;
+				// act
+				output = numbers.add(input1, input2);
+				// assert using matchers
+				expect(output).toEqual(3);
+				expect(output).not.toEqual(4);
+
+			});
+
+			it()
+
+			
+		});
+
+	});
+
+
+
+});
